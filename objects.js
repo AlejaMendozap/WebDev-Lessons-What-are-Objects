b44c0278a1af03712721ec35c0f415f03e8a279e
Bruce Johnson <johnswanson3.9@gmail.com>
2020-03-28 15:17:09 -0400

    convert carsObject to an array

--- objects.js
+++ objects.js
@@ -1,21 +1,19 @@
-var carsObject = {
-  car1: {
-    make: 'Ford',
-    model: 'Fiesta',
-    drive: 'TODO',
-    location: [0, 0],
-  },
-};
+const cars = [{
+  make: 'Ford',
+  model: 'Fiesta',
+  drive: 'TODO',
+  location: [0, 0],
+}];
 
 /**
-1. Write a function for the drive key in carsObject that takes in three arguments (x, y, car) and changes the location
+1. Write a function for the drive key in cars that takes in three arguments (x, y, car) and changes the location
 of the the car specified by the third argument. Figure out how you can alter the location of the car using only
 the key:value pairs given above. If you run into issues, try building the function in the object itself.
 
 Example:
 
-carsObject.car1.drive(3, 4, car1);
-carsObject.car1.location -> [3, 4]
+cars[0].drive(3, 4, cars[0]);
+cars[0].location -> [3, 4]
 
 HINT: you can use hard brackets [] to indicate to JavaScript that the following is a variable and NOT
 an exact key name.
@@ -35,10 +33,10 @@ Example:
 
 */
 
-// 2. Add a property to car1 called year and give it a value of '2003'
+// 2. Add a key to the first car object in the cars array called year and give it a value of '2003'
 
 /**
 3. Write a function called editCars that allows the user to specify a key they want to change, the value they want
 to change it to, and the car these changes should be made on (key, value, car). Assume every car specified by the car argument is
-within carsObject.
+within the cars array.
 */
